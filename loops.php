b636f9f6e907361138bee3c2e09975eac5ab47f6
Le-Yo <lenykoskey@yahoo.com>
2015-08-25 19:32:40 +0300

    added for and foreach loops

---
+++ loops.php
@@ -0,0 +1,17 @@
+<?php
+
+$week_days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+
+
+// foreach ($week_days as $day) {
+//   # code...
+//   echo $day."<br>";
+// }
+
+for ($i=0; $i <=count($week_days) ; $i++) {
+  # code...
+  echo $week_days[$i]."<br>";
+}
+
+
+ ?>
