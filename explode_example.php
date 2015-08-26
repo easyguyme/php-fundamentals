1543f9a95211adcf2123015650c8bd323a3231fc
Le-Yo <lenykoskey@yahoo.com>
2015-08-26 12:48:59 +0300

    added powerpoint presentation

--- explode_example.php
+++ explode_example.php
@@ -1,7 +1,7 @@
 <?php
 $sms = "Leonard#32749387#Nailab";
 
-$exploded_text = explode('#',$sms);
+$exploded_text = explode('a',$sms);
 
 $name = $exploded_text[0];
 
@@ -9,7 +9,7 @@ $index_number = $exploded_text[1];
 
 $school = $exploded_text[2];
 
-print_r($index_number);
+print_r($exploded_text);
 
 
 
